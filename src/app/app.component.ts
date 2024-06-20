999cfcdf3141957423680c4b4569f78edffa67fa
Alex Barreto <alexbarreto2020@hotmail.com>
2024-06-19 22:01:39 -0300

    fist commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.sass'
+})
+export class AppComponent {
+  title = 'angular_sass';
+}
